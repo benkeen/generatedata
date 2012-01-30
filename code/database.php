56fa923cc0bcb01382c7c176001b1d92503fc802
Matt Burke <MattBurke@quickenloans.com>
2012-01-30 17:00:56 -0500

    Add more error detail when database connection fails

--- code/database.php
+++ code/database.php
@@ -9,9 +9,9 @@ function gd_db_connect()
 	global $g_table_prefix, $g_db_hostname, $g_db_username, $g_db_password, $g_db_name;
 
 	$link = mysql_connect($g_db_hostname, $g_db_username, $g_db_password)
-		or die("Couldn't connect to database.");
+		or die("Couldn't connect to database: " . mysql_error());
 	@mysql_select_db($g_db_name)
-		or die ("couldn't find database '$g_db_name'.");
+		or die ("couldn't find database '$g_db_name': " . mysql_error());
 
 	return $link;
 }
