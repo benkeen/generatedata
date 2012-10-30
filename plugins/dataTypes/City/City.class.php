ba66cc2f682792039b5e0ff32b698fe43ff45cb5
Ben Keen <ben.keen@gmail.com>
2012-10-29 21:21:25 -0700

    City data type added

--- plugins/dataTypes/City/City.class.php
+++ plugins/dataTypes/City/City.class.php
@@ -6,18 +6,28 @@ class DataType_City extends DataTypePlugin {
 	protected $dataTypeFieldGroup = "geo";
 	protected $dataTypeFieldGroupOrder = 20;
 	protected $processOrder = 3;
-	private $cities;
+	private $citiesByRegion;
+	private $numRegions;
+
 
 	public function __construct($runtimeContext) {
+		// call the parent constructor (populates $L)
+		parent::__construct();
 
+		// if we're in the process of generating data, populate the private vars with the first and last names
+		// needed for data generation
+		if ($runtimeContext == "generation") {
+			self::initCityList();
+		}
 	}
 
 
 	public function generate($row, $placeholderStr, $existingRowData) {
-		global $City_list;
 
 		// see if this row has a region [N.B. This is something that could be calculated ONCE on the first row]
-		$row_region_info = array();
+		$rowRegionInfo = array();
+		$rowCountryInfo = array();
+		/*
 		while (list($key, $info) = each($existing_row_data)) {
 			if ($info["data_type_folder"] == "StateProvince") {
 				$row_region_info = $info;
@@ -36,23 +46,56 @@ class DataType_City extends DataTypePlugin {
 				}
 			}
 		}
+		*/
 
-		$random_city = "";
-		if (!empty($row_region_info)) {
-			$region_id = $row_region_info["random_data"]["region_id"];
-			$random_city = $City_list[$region_id][rand(0, count($City_list[$region_id])-1)]["city"];
-		} else if (!empty($row_country_info)) {
+		$randomCity = "";
+/*		if (!empty($rowRegionInfo)) {
+			$regionSlug = $rowRegionInfo["randomData"]["region_slug"];
+			$randomCity = $this->citiesByRegion[$regionSlug][rand(0, count($this->citiesByRegion[$regionSlug])-1)]["city"];
+		} else if (!empty($rowCountryInfo)) {
 			// get all region IDs associated with this country
-			$regions = Country_get_regions($row_country_info["random_data"]["id"]);
+			$regions = Country_get_regions($row_country_info["randomData"]["id"]);
 			$random_region_id = $regions[rand(0, count($regions)-1)]["region_id"];
 			$random_city = $City_list[$random_region_id][rand(0, count($City_list[$random_region_id])-1)]["city"];
 		} else {
-			$rand_region_id = array_rand($City_list);
-			$random_city = $City_list[$rand_region_id][rand(0, count($City_list[$rand_region_id])-1)]["city"];
+		*/
+			$randRegionSlug = array_rand($this->citiesByRegion);
+			$randomCity = $this->citiesByRegion[$randRegionSlug][rand(0, count($this->citiesByRegion[$regionSlug])-1)]["city"];
+		//}
+
+		return $randomCity;
+	}
+
+
+	/**
+	 * Called when the plugin is initialized during data generation.
+	 */
+	private function initCityList() {
+		$prefix = Core::getDbTablePrefix();
+		$response = Core::$db->query("
+			SELECT *
+			FROM {$prefix}cities
+		");
+
+		if ($response["success"]) {
+			$cities = array();
+			$citiesByRegion = array();
+			while ($cityInfo = mysql_fetch_assoc($response["results"])) {
+				if (!array_key_exists($cityInfo["region_slug"], $citiesByRegion)) {
+					$citiesByRegion[$cityInfo["region_slug"]] = array();
+				}
+
+				$citiesByRegion[$cityInfo["region_slug"]][] = array(
+					"city"    => $cityInfo["city"],
+					"city_id" => $cityInfo["city_id"]
+				);
+			}
 		}
 
-		return $random_city;
+		$this->citiesByRegion = $citiesByRegion;
+		$this->numRegions = count($citiesByRegion);
 	}
+
 
 	public function getExportTypeInfo($exportType, $options) {
 		$info = "";
@@ -67,30 +110,4 @@ class DataType_City extends DataTypePlugin {
 
 		return $info;
 	}
-
-	// NAH! Move this to a constructor, then make a simple getter
-	public function getList() {
-		global $g_table_prefix;
-
-		$query = mysql_query("
-			SELECT *
-			FROM   {$g_table_prefix}cities
-		");
-
-		$cities = array();
-		$cities_by_region = array();
-		while ($city_info = mysql_fetch_assoc($query))
-		{
-			if (!array_key_exists($city_info["region_id"], $cities_by_region))
-				$cities_by_region[$city_info["region_id"]] = array();
-
-			$cities_by_region[$city_info["region_id"]][] = array(
-				"city"    => $city_info["city"],
-				"city_id" => $city_info["city_id"]
-			);
-		}
-
-		return $cities_by_region;
-	}
-
 }
