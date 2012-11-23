db6c113bb82fedb266a2b916f756ab2a1885c989
Ben Keen <ben.keen@gmail.com>
2012-11-22 22:36:38 -0800

    More synching

--- plugins/exportTypes/XML/XML.class.php
+++ plugins/exportTypes/XML/XML.class.php
@@ -28,36 +28,26 @@ class XML extends ExportTypePlugin {
 		$html =<<< END
 <table cellspacing="0" cellpadding="0" width="100%">
 <tr>
-	<td width="30%" valign="top">
+	<td width="40%" valign="top">
 		<table cellspacing="0" cellpadding="0">
 		<tr>
-			<td width="160"><label for="etXML_rootNodeName">{$LANG["root_node_name"]}</label></td>
+			<td width="160"><label for="etXMLRootNodeName">{$LANG["root_node_name"]}</label></td>
 			<td>
-				<input type="text" size="15" name="etXML_rootNodeName" id="etXML_rootNodeName" value="records" />
+				<input type="text" size="15" name="etXMLRootNodeName" id="etXMLRootNodeName" value="records" />
 			</td>
 		</tr>
 		<tr>
-			<td><label for="etXML_recordNodeName">{$LANG["record_node_name"]}</label></td>
+			<td><label for="etXMLRecordNodeName">{$LANG["record_node_name"]}</label></td>
 			<td>
-				<input type="text" size="15" name="etXML_recordNodeName" id="etXML_recordNodeName" value="record" />
+				<input type="text" size="15" name="etXMLRecordNodeName" id="etXMLRecordNodeName" value="record" />
 			</td>
 		</tr>
 		</table>
 	</td>
-	<td width="70%" valign="top">
-		<label for="etXML_useCustomExportFormat">
-			<input type="checkbox" name="etXML_useCustomExportFormat" id="etXML_useCustomExportFormat" />
-			{$LANG["use_custom_xml_format"]}
-		</label>
-		<textarea style="width: 98%; height: 100px" class="gdDisabled" name="etXML_customFormat" id="etXML_customFormat" disabled="disabled">&lt;?xml version="1.0" encoding="UTF-8" ?&gt;
-&lt;records&gt;
-	{records}
-		&lt;record&gt;
-			&lt;row1&gt;{ROW1}&lt;/row1&gt;
-			&lt;row2&gt;{ROW2}&lt;/row2&gt;
-		&lt;/record&gt;
-	{/records}
-&lt;/records&gt;</textarea>
+	<td width="60%" valign="top">
+		<input type="checkbox" name="etXMLUseCustomExportFormat" id="etXMLUseCustomExportFormat" />
+			<label for="etXMLUseCustomExportFormat">{$this->L["use_custom_xml_format"]}</label>
+			<input type="button" id="etXMLEditCustomFormat" value="edit" disabled="disabled" />
 	</td>
 </tr>
 </table>
@@ -123,5 +113,4 @@ END;
 
 		return $final_xml;
 	}
-
 }
--- plugins/exportTypes/XML/lang/en.php
+++ plugins/exportTypes/XML/lang/en.php
@@ -1,6 +1,6 @@
 <?php
 
 $L = array();
-
+$L["use_custom_xml_format"] = "Use custom XML format";
 $L["row_label"] = "Node Name";
 $L["row_label_plural"] = "Node Names";
