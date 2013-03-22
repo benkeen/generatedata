e36b2147650526236462f4bb9dc532951de0885c
Ben Keen <ben.keen@gmail.com>
2013-03-21 20:59:51 -0700

    Standard Normal Deviation update

--- plugins/dataTypes/NormalDistribution/NormalDistribution.class.php
+++ plugins/dataTypes/NormalDistribution/NormalDistribution.class.php
@@ -10,7 +10,7 @@ class DataType_NormalDistribution extends DataTypePlugin {
      * @access protected
      */
 	protected $isEnabled = true;
-	protected $dataTypeName = "Normal Distribution";
+	protected $dataTypeName = "Standard Normal Distribution";
 	protected $dataTypeFieldGroup = "math";
 	protected $dataTypeFieldGroupOrder = 10;
 	protected $jsModules = array("NormalDistribution.js");
@@ -18,20 +18,21 @@ class DataType_NormalDistribution extends DataTypePlugin {
 
 
 	public function generate($generator, $generationContextData) {
-		$mean  = $generationContextData["mean"];
-		$sigma = $generationContextData["sigma"];
+		$mean  = (float) $generationContextData["generationOptions"]["mean"];
+		$sigma = (float) $generationContextData["generationOptions"]["sigma"];
+		$randNum = $this->getNormallyDistributedRandomNum();
+		$num = ($randNum * $sigma) + $mean;
 
 		return array(
-			"display" => $this->gauss($mean, $sigma)
+			"display" => $num
 		);
 	}
 
 	public function getRowGenerationOptions($generator, $postdata, $colNum, $numCols) {
-		if (!isset($postdata["dtOptionMean_$colNum"]) || empty($postdata["dtOptionMean_$colNum"]) ||
-			!isset($postdata["dtOptionSigma_$colNum"]) || empty($postdata["dtOptionSigma_$colNum"])) {
+		if ((empty($postdata["dtOptionMean_$colNum"]) && $postdata["dtOptionMean_$colNum"] !== "0") ||
+			(empty($postdata["dtOptionSigma_$colNum"]) && $postdata["dtOptionSigma_$colNum"] !== "0")) {
 			return false;
 		}
-
 		$this->randMax = (float) getrandmax();
 
 		return array(
@@ -43,43 +44,24 @@ class DataType_NormalDistribution extends DataTypePlugin {
 	public function getOptionsColumnHTML() {
 		$options =<<< END
 			<label for="dtOptionMean_%ROW%">Mean</label>
-				<input type="text" name="dtOptionMean_%ROW%" id="dtOptionMean_%ROW%" style="width: 30px" value="45" />
-			<label for="dtOptionSigma_%ROW%">Sigma</label>
+				<input type="text" name="dtOptionMean_%ROW%" id="dtOptionMean_%ROW%" style="width: 30px" value="0" />
+			<label for="dtOptionSigma_%ROW%">Standard Deviation</label>
 				<input type="text" name="dtOptionSigma_%ROW%" id="dtOptionSigma_%ROW%" style="width: 30px" value="10" />
 END;
 
 		return $options;
 	}
 
-	// N(m,s)
-	// returns random number with normal distribution:
-	//    mean=m
-	//    std dev=s
-	public function gauss_ms($m = 0.0, $s = 1.0) {
-		return $this->gauss() * $s + $m;
+
+	// returns random number with normal distribution
+	public function getNormallyDistributedRandomNum() {
+		$num1 = $this->getRandomNum();
+		$num2 = $this->getRandomNum();
+		return sqrt(-2 * log($num1)) * cos(2 * pi() * $num2);
 	}
 
-	public function gauss() {
-		// N(0,1)
-		// returns random number with normal distribution:
-		//    mean = 0
-		//    std dev = 1
-
-		// auxilliary vars
-		$x = $this->random_0_1();
-		$y = $this->random_0_1();
-
-		// two independent variables with normal distribution N(0,1)
-		$u = sqrt(-2 * log($x)) * cos(2 * pi() * $y);
-//		$v = sqrt(-2 * log($x)) * sin(2 * pi() * $y);
-
-		// i will return only one, couse only one needed
-		return $u;
-	}
-
-	// auxilliary function
-	// returns random number with flat distribution from 0 to 1
-	public function random_0_1() {
+	// returns a random float from 0 to 1
+	public function getRandomNum() {
 		return (float) rand() / $this->randMax;
 	}
 }
