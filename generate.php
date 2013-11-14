8d21dac92c90e37e56edc85d8b57f8316f07d73f
Code Manure <unullmass@users.noreply.github.com>
2013-11-14 10:18:10 +0530

    Update generate.php with compression
    Generatedata sometimes provides files that are several MB in size. In order to save on bandwidth costs, data files over 500 KB will be compressed to zip. 100000 rows of data consisting of about 5 columns of data is about 6 MB uncompressed, and a little over 2 MB when not. That's over 60% bandwidth saved.
    
    It would be great if we could add the compress option to the UI so that users would have a choice.

--- generate.php
+++ generate.php
@@ -8,11 +8,37 @@ $response = $gen->generate();
 
 if ($gen->getExportTarget() == "promptDownload") {
 	header("Cache-Control: private, no-cache, must-revalidate");
-	header("Content-Type: {$response["contentTypeHeader"]}");
+	
+	//you could check if the file size is more than 500 KB and if so zip the file
+	if((strlen($response["content"]) / 1000) > 500){
+		$filename=session_id()."_".$response["promptDownloadFilename"];
+		$filepath=$filename;
+		$zippath=$filepath.".zip";
+		if(file_put_contents($filepath,$response["content"])){
+			//now that you've written the file proceed to zip it
+			$zip = new ZipArchive();
+			//we'll use the session key of the user to keep it clean. no mixups of datasets. hopefully they won't try this with multiple tabs!
+			$zipfile = $zip->open($zippath,ZipArchive::CREATE);
+			if($zipfile){
+				if ($zip->addFile($filepath,$response["promptDownloadFilename"])){
+					//we've got our zip file now we may set the response header
+					$zip->close();
+					header("Content-type: application/zip"); 
+					header("Content-Disposition: attachment; filename=".$response["promptDownloadFilename"].".zip");
+					readfile($zippath);
+					unlink($zippath);
+					unlink($filepath);
+					//exit sending the zip back
+					exit;
+				}
+			}
+		}
+	}
+	else{//no compression, send the original data back
+		header("Content-Type: {$response["contentTypeHeader"]}");
+		if (isset($response["promptDownloadFilename"]) && !empty($response["promptDownloadFilename"])) {
+        		header("Content-Disposition: attachment; filename={$response["promptDownloadFilename"]}");
+		}
+		echo $response["content"];
+	}
 }
-
-if (isset($response["promptDownloadFilename"]) && !empty($response["promptDownloadFilename"])) {
-	header("Content-Disposition: attachment; filename={$response["promptDownloadFilename"]}");
-}
-
-echo $response["content"];
