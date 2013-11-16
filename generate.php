af6f262919f029ce9b9e20bc3564151bde0eae53
Code Manure <unullmass@users.noreply.github.com>
2013-11-16 22:47:46 +0530

    Update generate.php
    All file types can be compressed now except for excel.

--- generate.php
+++ generate.php
@@ -10,7 +10,7 @@ if ($gen->getExportTarget() == "promptDownload") {
 	header("Cache-Control: private, no-cache, must-revalidate");
 	
 	//check if user selected the zip checkbox and zip
-	if($_POST["gdExportTarget_promptDownload_zip"]=="DoZip"){
+	if($gen->isPromptDownloadZipped()){
 		$filename=session_id()."_".$response["promptDownloadFilename"];
 		$filepath=$filename;
 		$zippath=$filepath.".zip";
