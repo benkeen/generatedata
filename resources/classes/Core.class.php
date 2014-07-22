6f36a61f1b0c9f5852621bb1efdd3314cc3b8a08
Ben Keen <ben.keen@gmail.com>
2014-07-21 18:30:21 -0700

    version update

--- resources/classes/Core.class.php
+++ resources/classes/Core.class.php
@@ -32,8 +32,8 @@ class Core {
 	private static $timeout = 300; // 5 minutes
 
 	// non-overridable settings
-	private static $version = "3.1.2";
-	private static $releaseDate = "2014-07-12";
+	private static $version = "3.1.3";
+	private static $releaseDate = "2014-07-20";
 	private static $minimumPHPVersion = "5.3.0";
 	private static $minimumMySQLVersion = "4.1.3";
 	private static $settingsFileExists = false;
