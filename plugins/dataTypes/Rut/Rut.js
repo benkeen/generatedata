f6db0167d15ebad95baaf81097f2430596fcde77
NioZero <ghidalgo@frostdisk.com>
2015-08-05 11:43:39 -0300

    Fixing save & load configuration
    - Fixed js to load saved configuration

--- plugins/dataTypes/Rut/Rut.js
+++ plugins/dataTypes/Rut/Rut.js
@@ -31,42 +31,39 @@ define([
 	var _loadRow = function(rowNum, data) {
 		return {
 			execute: function() {},
-			isComplete: function() {
-				if ($("#dtOption_" + rowNum).length) {
-					$("#dtExample_" + rowNum).val(data.formatCode);
-					
-					if ($("#dtThouSep_" + rowNum).length) {
-						if (data.thousep) {
-							$("#dtThouSep_" + rowNum).attr("checked", "checked");
-						} else {
-							$("#dtThouSep_" + rowNum).removeAttr("checked");
-						}
-					}
-					if ($("#dtUpperDigit_" + rowNum).length) {
-						if (data.upper) {
-							$("#dtUpperDigit_" + rowNum).attr("checked", "checked");
-						} else {
-							$("#dtUpperDigit_" + rowNum).removeAttr("checked");
-						}
-					}
-					if ($("#dtRemoveDash_" + rowNum).length) {
-						if (data.remdash) {
-							$("#dtRemoveDash_" + rowNum).attr("checked", "checked");
-						} else {
-							$("#dtRemoveDash_" + rowNum).removeAttr("checked");
-						}
-					}
-					
-				}
-				
-				return true;
+			isComplete: function () {
+			    $("#dtExample_" + rowNum).val(data.example);
+
+			    if ($("#dtThouSep_" + rowNum).length) {
+			        if (data.thousep) {
+			            $("#dtThouSep_" + rowNum).attr("checked", "checked");
+			        } else {
+			            $("#dtThouSep_" + rowNum).removeAttr("checked");
+			        }
+			    }
+			    if ($("#dtUpperDigit_" + rowNum).length) {
+			        if (data.upper) {
+			            $("#dtUpperDigit_" + rowNum).attr("checked", "checked");
+			        } else {
+			            $("#dtUpperDigit_" + rowNum).removeAttr("checked");
+			        }
+			    }
+			    if ($("#dtRemoveDash_" + rowNum).length) {
+			        if (data.remdash) {
+			            $("#dtRemoveDash_" + rowNum).attr("checked", "checked");
+			        } else {
+			            $("#dtRemoveDash_" + rowNum).removeAttr("checked");
+			        }
+			    }
+
+			    return true;
 			}
 		};
 	};
 
 	var _saveRow = function(rowNum) {
 		return {
-			"formatCode":  $("#dtExample_" + rowNum).val(),
+		    "example": $("#dtExample_" + rowNum).val(),
 			"thousep": ($("#dtThouSep_" + rowNum).attr("checked")) ? "checked" : "",
 			"upper": ($("#dtUpperDigit_" + rowNum).attr("checked")) ? "checked" : "",
 			"remdash": ($("#dtRemoveDash_" + rowNum).attr("checked")) ? "checked" : ""
