d066027c66262c6f1666d077c055e1414a2ce1a8
Conrad Hagemans <conrad@hagemans.com>
2015-12-14 01:09:07 +0100

    Updated Dutch translations

--- plugins/dataTypes/AutoIncrement/lang/nl.php
+++ plugins/dataTypes/AutoIncrement/lang/nl.php
@@ -4,12 +4,12 @@ $L = array();
 
 $L["DATA_TYPE"] = array(
     "NAME" => "Auto-increment",
-    "DESC" => "Biedt een groot aantal variaties op een standaard auto- increment veld : aangepaste beginnen waarde , aanpasbare stap maten , willekeurige reeks opnemen , verhogen of verlagen en meer."
+    "DESC" => "Biedt een groot aantal variaties op een standaard auto-increment veld : aangepaste begin waarde , aanpasbare stap maten , willekeurige reeks opnemen , verhogen of verlagen en meer."
 );
 
-$L["help_intro"] = "Genereert een kolom die een uniek nummer op elke rij bevat, verhogen met welke waarde die u invoert. Deze optie kan nuttig zijn voor het inbrengen van de gegevens in een database veld met een auto-increment primaire sleutel.";
-$L["help_para2"] = "De optionele tijdelijke aanduiding snaar kunt u insluiten de gegenereerde toename waarde binnen een string, via de <b>{\$INCR}</b> tijdelijke aanduiding. Bijvoorbeeld:";
-$L["incomplete_fields"] = "Vul de Start At en Increment velden voor alle Auto-increment rijen:";
+$L["help_intro"] = "Genereert een kolom die een uniek nummer in elke rij opneemt, verhoogt met de waarde die u hebt ingevoerd. Deze optie kan nuttig zijn voor het inlezen van de gegevens in een database dat een veld met een auto-increment bevat (unieke sleutel).";
+$L["help_para2"] = "Met de optionele placeholderstring kunt u de gegenereerde waarde opnemen als onderdeel van een string. Gebruik hiervoor de <b>{\$INCR}</b> binnen de string. Bijvoorbeeld:";
+$L["incomplete_fields"] = "Vul de Startwaarde en Increment velden voor alle Auto-increment rijen:";
 $L["increment_c"] = "Increment:";
 $L["placeholder_str"] = "Placeholder string:";
-$L["start_at_c"] = "Start bij:";
+$L["start_at_c"] = "Start met:";
