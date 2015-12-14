86878196bcb78e995045bc53df169246a009e0db
Conrad Hagemans <conrad@hagemans.com>
2015-12-14 01:13:15 +0100

    Updated Dutch translations

--- plugins/dataTypes/Company/lang/nl.php
+++ plugins/dataTypes/Company/lang/nl.php
@@ -3,6 +3,6 @@
 $L = array();
 
 $L["DATA_TYPE"] = array(
-    "NAME" => "Vennootschap",
-    "DESC" => "Dit gegevenstype genereert een willekeurige naam, bestaande uit een Lorem ipsum woord en een passende achtervoegsel, zoals Dolor Inc, of Convallis Limited."
+    "NAME" => "Bedrijfsnaam",
+    "DESC" => "Dit gegevenstype genereert een willekeurige bedrijfsnaam, bestaande uit een Lorem ipsum woord en een passend achtervoegsel, zoals bijvoorbeeld Dolor Inc, of Convallis Limited."
 );
