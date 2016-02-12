efc6a26fc9b89e15c01c8c02955ee845edaa9f3a
Ben Keen <ben.keen@gmail.com>
2016-02-11 17:47:09 -0800

    minor text fixes

--- plugins/dataTypes/Boolean/lang/en.php
+++ plugins/dataTypes/Boolean/lang/en.php
@@ -9,9 +9,9 @@ $L["DATA_TYPE"] = array(
 
 $L["help_intro"] = "You can specify multiple formats by separating them with the pipe (|) character. The following strings will be converted to their Boolean equivalent:";
 $L["incomplete_fields"] = "The Boolean data type needs to have the format entered in the Options text field. Please fix the following rows:";
-$L["example_YesNo"] = "Yes of No";
-$L["example_FalseTrue"] = "False of True";
-$L["example_ZeroOne"] = "0 of 1";
-$L["example_YesNoShort"] = "Y of N";
-$L["example_FalseTrueShort"] = "F of T";
-$L["example_FalseTrueLower"] = "false of true";
+$L["example_YesNo"] = "Yes or No";
+$L["example_FalseTrue"] = "False or True";
+$L["example_FalseTrueLower"] = "false or true";
+$L["example_ZeroOne"] = "0 or 1";
+$L["example_YesNoShort"] = "Y or N";
+$L["example_FalseTrueShort"] = "F or T";
