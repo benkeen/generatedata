33dfa7c830312a28b581fe8d432500aeb1ecb790
Ben Keen <ben.keen@gmail.com>
2019-11-26 20:41:15 -0800

    Added store

--- src/app.js
+++ src/app.js
@@ -1,24 +1,28 @@
 import React from 'react';
+import { Provider } from 'react-redux';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import store from './store';
 import Page from './components/page/Page.component';
 
 
 const App = () => (
-	<Router>
-		<Page>
-			<Switch>
-				<Route path="/about">
-					<div>About</div>
-				</Route>
-				<Route path="/users">
-					<div>Users</div>
-				</Route>
-				<Route path="/">
-					<div>HOME</div>
-				</Route>
-			</Switch>
-		</Page>
-	</Router>
+	<Provider store={store}>
+		<Router>
+			<Page>
+				<Switch>
+					<Route path="/about">
+						<div>About</div>
+					</Route>
+					<Route path="/users">
+						<div>Users</div>
+					</Route>
+					<Route path="/">
+						<div>HOME</div>
+					</Route>
+				</Switch>
+			</Page>
+		</Router>
+	</Provider>
 );
 
 export default App;
--- src/store/index.js
+++ src/store/index.js
@@ -1,1 +1,52 @@
-// init the redux store here
+import thunk from 'redux-thunk';
+import reducerRegistry from './reducerRegistry';
+import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
+
+const initialState = {};
+
+// preserve initial state for not-yet-loaded reducers
+const combine = (reducers) => {
+	const reducerNames = Object.keys(reducers);
+	Object.keys(initialState).forEach(item => {
+		if (reducerNames.indexOf(item) === -1) {
+			reducers[item] = (state = null) => state;
+		}
+	});
+	return combineReducers(reducers);
+};
+
+
+function initStore (initialState) {
+	let middleware = [thunk];
+	let enhancers = [];
+	let composeEnhancers = compose;
+
+	if (process.env.NODE_ENV === 'development') {
+		const composeWithDevToolsExtension = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__;
+		if (typeof composeWithDevToolsExtension === 'function') {
+			composeEnhancers = composeWithDevToolsExtension;
+		}
+	}
+
+	const topLevelReducer = combine(reducerRegistry.getReducers());
+	const store = createStore(
+		topLevelReducer,
+		initialState,
+		composeEnhancers(
+			applyMiddleware(...middleware),
+			...enhancers
+		)
+	);
+	store.asyncReducers = {};
+
+	return store;
+}
+
+
+const store = initStore({});
+
+// allows dynamically changing the redux store
+reducerRegistry.setChangeListener(reducers => store.replaceReducer(combine(reducers)));
+
+export default store;
+
