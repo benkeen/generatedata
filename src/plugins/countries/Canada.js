2d2336bde93a9db6912fd6dcf0f24da78bb69f5f
Ben Keen <ben.keen@gmail.com>
2019-11-28 23:05:41 -0800

    moved country data to JS

--- src/plugins/countries/Canada.js
+++ src/plugins/countries/Canada.js
@@ -1,37 +1,30 @@
-<?php
+export default {
+	"countryName": "Brazil",
+	"countrySlug": "brazil",
+	"regionNames": "Brazilian States",
+	"continent": "south_america",
+	"extendedData": {
 
-/**
- * @package Countries
- */
-
-class Country_Canada extends CountryPlugin
-{
-	protected $continent = "north_america";
-	protected $countryName = "Canada";
-	protected $countrySlug = "CA";
-	protected $regionNames = "Canadian Provinces";
-
-	protected $extendedData = array(
 		// the general zip format for the country. This may be optionally overridden for each region if a more
 		// specific format is desired. To prevent duplicate code, the replacements listed here cover ALL zip formats
 		// for each province
-		"zipFormat" => array(
-			"format" => "%*@ *@*",
-			"replacements" => array(
-				"%" => "ABCEGHJKLMNPRSTVXY",
-				"*" => "0123456789",
-				"@" => "ABCEGHJKLMNPRSTVWXYZ",
+		"zipFormat": {
+			"format": "%*@ *@*",
+			"replacements": {
+				"%": "ABCEGHJKLMNPRSTVXY",
+				"*": "0123456789",
+				"@": "ABCEGHJKLMNPRSTVWXYZ",
 
 				// used in individual provinces below
-				"&" => "GHJ",   // QC
-				"^" => "KLMNP"  // ON
-			)
-		),
+				"&": "GHJ",   // QC
+				"^": "KLMNP"  // ON
+			}
+		},
 
 		// the general phone format and area codes for the country
 		// http://cnac.ca/area_code_maps/canadian_area_codes.htm
-		"phoneFormat" => array(
-			"areaCodes" => array(
+		"phoneFormat": {
+			"areaCodes": [
 				403, 587, 780, 825,		// AB
 				236, 250, 604, 672, 778,	// BC
 				204, 431,			// MB
@@ -43,23 +36,23 @@ class Country_Canada extends CountryPlugin
 				782, 902,			// PE
 				367, 418, 581, 450, 579, 438, 514, 819, 873,				// QC
 				306, 639			// SK
-			),
-			"displayFormats" => array(
-				"(AAA) Xxx-xxxx",
-				"1 (AAA) Xxx-xxxx",
+			],
+			"displayFormats": [
+				"(AAA} Xxx-xxxx",
+				"1 (AAA} Xxx-xxxx",
 				"1-AAA-Xxx-xxxx"
-			)
-		)
-	);
+			]
+		}
+	},
 
 	// our country-wide data, with info separated into regions
-	protected $countryData = array(
-		array(
-			"regionName" => "Alberta",
-			"regionShort" => "AB",
-			"regionSlug" => "alberta",
-			"weight" => 11,
-			"cities" => array(
+	data: [
+		{
+			"regionName": "Alberta",
+			"regionShort": "AB",
+			"regionSlug": "alberta",
+			"weight": 11,
+			"cities": [
 				"Airdrie", "Alix", "Banff", "Barrhead", "Bearberry", "Beaumont", "Bon Accord", "Bonnyville",
 				"Bonnyville Municipal District", "Bowden", "Breton", "Bruderheim", "Calgary", "Calmar", "Camrose",
 				"Canmore", "Carstairs", "Castor", "Chestermere", "Clearwater Municipal District", "Coaldale", "Coalhurst",
@@ -73,22 +66,22 @@ class Country_Canada extends CountryPlugin
 				"St. Paul", "Sedgewick", "Smoky Lake", "Spruce Grove", "Stirling", "Strathcona County", "Stony Plain", "Sundrie",
 				"Sunset Point", "Swan Hills", "Sylvan Lake", "Taber", "Tofield", "Trochu", "Valleyview", "Vegreville", "Vilna",
 				"Wabamun", "Warburg", "Warspite", "Westlock", "Wetaskiwin", "Wood Buffalo", "Woodlands County", "Yellowhead County"
-			),
-			"extendedData" => array(
-				"zipFormat" => array(
-					"format" => "T*@ *@*"
-				),
-				"phoneFormat" => array(
-					"areaCodes" => array(403, 587, 780, 825)
-				)
-			)
-		),
-		array(
-			"regionName" => "British Columbia",
-			"regionShort" => "BC",
-			"regionSlug" => "british_columbia",
-			"weight" => 13,
-			"cities" => array(
+			],
+			"extendedData": {
+				"zipFormat": {
+					"format": "T*@ *@*"
+				},
+				"phoneFormat": {
+					"areaCodes": [403, 587, 780, 825]
+				}
+			}
+		},
+		{
+			"regionName": "British Columbia",
+			"regionShort": "BC",
+			"regionSlug": "british_columbia",
+			"weight": 13,
+			"cities": [
 				"100 Mile House", "Abbotsford", "Alert Bay", "Armstrong", "Belcarra", "Burnaby", "Burns Lake", "Cache Creek",
 				"Cariboo Regional District", "Castlegar", "Chetwynd", "Chilliwack", "Coldstream", "Colwood", "Comox", "Coquitlam",
 				"Cranbrook", "Dawson Creek", "Delta", "Fernie", "Duncan", "Fort St. John", "Fraser Lake", "Fraser-Fort George",
@@ -99,130 +92,130 @@ class Country_Canada extends CountryPlugin
 				"Qualicum Beach", "Richmond", "Salt Spring Island", "Silverton", "Smithers", "Sooke", "Sparwood", "Stewart",
 				"Sunshine Coast Regional District", "Surrey", "Terrance", "Tumbler Ridge", "Ucluelet", "Vancouver", "Vanderhoof",
 				"Victoria", "West Vancouver", "White Rock", "Williams Lake"
-			),
-			"extendedData" => array(
-				"zipFormat" => array(
-					"format" => "V*@ *@*"
-				),
-				"phoneFormat" => array(
-					"areaCodes" => array(236, 250, 604, 672, 778)
-				)
-			)
-		),
-		array(
-			"regionName" => "Manitoba",
-			"regionShort" => "MB",
-			"regionSlug" => "manitoba",
-			"weight" => 4,
-			"cities" => array(
+			],
+			"extendedData": {
+				"zipFormat": {
+					"format": "V*@ *@*"
+				},
+				"phoneFormat": {
+					"areaCodes": [236, 250, 604, 672, 778]
+				}
+			}
+		},
+		{
+			"regionName": "Manitoba",
+			"regionShort": "MB",
+			"regionSlug": "manitoba",
+			"weight": 4,
+			"cities": [
 				'Winnipeg', 'Stonewall', 'Minitonas', 'Lourdes', 'Flin Flon', 'Daly', 'Brandon', 'Beausejour'
-			),
-			"extendedData" => array(
-				"zipFormat" => array(
-					"format" => "R*@ *@*"
-				),
-				"phoneFormat" => array(
-					"areaCodes" => array(204, 431)
-				)
-			)
-		),
-		array(
-			"regionName" => "New Brunswick",
-			"regionShort" => "NB",
-			"regionSlug" => "new_brunswick",
-			"weight" => 2,
-			"cities" => array(
+			],
+			"extendedData": {
+				"zipFormat": {
+					"format": "R*@ *@*"
+				},
+				"phoneFormat": {
+					"areaCodes": [204, 431]
+				}
+			}
+		},
+		{
+			"regionName": "New Brunswick",
+			"regionShort": "NB",
+			"regionSlug": "new_brunswick",
+			"weight": 2,
+			"cities": [
 				"Bathurst", "Campbellton", "Dieppe", "Edmundston", "Fredericton", "Miramichi", "Moncton", "Saint John"
-			),
-			"extendedData" => array(
-				"zipFormat" => array(
-					"format" => "E*@ *@*"
-				),
-				"phoneFormat" => array(
-					"areaCodes" => array(428, 506)
-				)
-			)
-		),
-		array(
-			"regionName" => "Newfoundland and Labrador",
-			"regionShort" => "NL",
-			"regionSlug" => "newfoundland_and_labrador",
-			"weight" => 2,
-			"cities" => array(
+			],
+			"extendedData": {
+				"zipFormat": {
+					"format": "E*@ *@*"
+				},
+				"phoneFormat": {
+					"areaCodes": [428, 506]
+				}
+			}
+		},
+		{
+			"regionName": "Newfoundland and Labrador",
+			"regionShort": "NL",
+			"regionSlug": "newfoundland_and_labrador",
+			"weight": 2,
+			"cities": [
 				'St. John\'s', 'Springdale', 'Spaniard\'s Bay', 'Rigolet', 'Paradise', 'Mount Pearl', 'McCallum', 'Marystown',
 				'Harbour Grace', 'Glovertown', 'Gander', 'Fogo', 'Fortune', 'Carbonear', 'Burin', 'Bonavista', 'Bay Roberts'
-			),
-			"extendedData" => array(
-				"zipFormat" => array(
-					"format" => "A*@ *@*"
-				),
-				"phoneFormat" => array(
-					"areaCodes" => array(709, 879)
-				)
-			)
-		),
-		array(
-			"regionName" => "Northwest Territories",
-			"regionShort" => "NT",
-			"regionSlug" => "northwest_territories",
-			"weight" => 1,
-			"cities" => array(
+			],
+			"extendedData": {
+				"zipFormat": {
+					"format": "A*@ *@*"
+				},
+				"phoneFormat": {
+					"areaCodes": [709, 879]
+				}
+			}
+		},
+		{
+			"regionName": "Northwest Territories",
+			"regionShort": "NT",
+			"regionSlug": "northwest_territories",
+			"weight": 1,
+			"cities": [
 				"Yellowknife", "Wrigley", "Wha Ti", "Wekweti", "Tulita", "Tuktoyaktuk", "Tsiigehtchic", "Sachs Harbour",
 				"Rae Lakes", "Rae-Edzo", "Paulatuk", "Norman Wells", "Lutsel K'e", "Kakisa", "Inuvik", "Holman", "Hay River",
 				"Fort Smith", "Fort Simpson", "Fort Resolution", "Fort Providence", "Fort McPherson", "Fort Laird",
 				"Fort Good Hope", "Enterprise", "Deline", "Coleville Lake", "Aklavik"
-			),
-			"extendedData" => array(
-				"zipFormat" => array(
-					"format" => "X*@ *@*"
-				),
-				"phoneFormat" => array(
-					"areaCodes" => array(867)
-				)
-			)
-		),
-		array(
-			"regionName" => "Nova Scotia",
-			"regionShort" => "NS",
-			"regionSlug" => "nova_scotia",
-			"weight" => 1,
-			"cities" => array(
+			],
+			"extendedData": {
+				"zipFormat": {
+					"format": "X*@ *@*"
+				},
+				"phoneFormat": {
+					"areaCodes": [867]
+				}
+			}
+		},
+		{
+			"regionName": "Nova Scotia",
+			"regionShort": "NS",
+			"regionSlug": "nova_scotia",
+			"weight": 1,
+			"cities": [
 				'Municipal District', 'Town of Yarmouth', 'Wolfville', 'Pugwash', 'Pictou', 'New Glasgow',
 				'Halifax', 'Guysborough', 'Cumberland County', 'Cape Breton Island', 'Berwick', 'Baddeck', 'Argyle',
 				'Annapolis Royal', 'Annapolis County'
-			),
-			"extendedData" => array(
-				"zipFormat" => array(
-					"format" => "B*@ *@*"
-				),
-				"phoneFormat" => array(
-					"areaCodes" => array(782, 902)
-				)
-			)
-		),
-		array(
-			"regionName" => "Nunavut",
-			"regionShort" => "NU",
-			"regionSlug" => "nunavut",
-			"weight" => 1,
-			"cities" => array(
+			],
+			"extendedData": {
+				"zipFormat": {
+					"format": "B*@ *@*"
+				},
+				"phoneFormat": {
+					"areaCodes": [782, 902]
+				}
+			}
+		},
+		{
+			"regionName": "Nunavut",
+			"regionShort": "NU",
+			"regionSlug": "nunavut",
+			"weight": 1,
+			"cities": [
 				"Arviat", "Cambridge Bay", "Gjoa Haven", "Pangnirtung", "Iqaluit"
-			),
-			"extendedData" => array(
-				"zipFormat" => array(
-					"format" => "X*@ *@*"
-				),
-				"phoneFormat" => array(
-					"areaCodes" => array(867)
-				)
-			)
-		),
-		array(
-			"regionName" => "Ontario",
-			"regionShort" => "ON",
-			"regionSlug" => "ontario",
-			"weight" => 39,
-			"cities" => array(
+			],
+			"extendedData": {
+				"zipFormat": {
+					"format": "X*@ *@*"
+				},
+				"phoneFormat": {
+					"areaCodes": [867]
+				}
+			}
+		},
+		{
+			"regionName": "Ontario",
+			"regionShort": "ON",
+			"regionSlug": "ontario",
+			"weight": 39,
+			"cities": [
 				"Ajax", "Aurora", "Ancaster Town", "Barrie", "Bath", "Blind River", "Burlington", "Caledon", "Cobourg",
 				"Cornwall", "Cumberland", "East Gwillimbury", "Essex", "Etobicoke", "Gloucester", "Goderich", "Grey County",
 				"Guelph", "Hamilton", "Hearst", "Kapuskasing", "Kawartha Lakes", "Kearny", "King Township", "Kingston",
@@ -233,39 +226,39 @@ class Country_Canada extends CountryPlugin
 				"Renfrew", "Richmond Hill", "Russell", "Scarborough", "St. Catharines", "St. Thomas", "Greater Sudbury",
 				"Tay", "Thorold", "Thunder Bay", "Toronto", "Valley East", "Vanier", "Vaughan", "Warwick", "Welland",
 				"Whitby", "Whitchurch-Stouffville", "Whitewater Region Township", "Wilmont", "Windsor", "Woodstock"
-			),
-			"extendedData" => array(
-				"zipFormat" => array(
-					"format" => "^*@ *@*"
-				),
-				"phoneFormat" => array(
-					"areaCodes" => array(416, 647, 437, 519, 226, 613, 343, 705, 249, 807, 905, 289, 365)
-				)
-			)
-		),
-		array(
-			"regionName" => "Prince Edward Island",
-			"regionShort" => "PE",
-			"regionSlug" => "prince_edward_island",
-			"weight" => 1,
-			"cities" => array(
+			],
+			"extendedData": {
+				"zipFormat": {
+					"format": "^*@ *@*"
+				},
+				"phoneFormat": {
+					"areaCodes": [416, 647, 437, 519, 226, 613, 343, 705, 249, 807, 905, 289, 365]
+				}
+			}
+		},
+		{
+			"regionName": "Prince Edward Island",
+			"regionShort": "PE",
+			"regionSlug": "prince_edward_island",
+			"weight": 1,
+			"cities": [
 				"Charlottetown", "Montague", "Stratford"
-			),
-			"extendedData" => array(
-				"zipFormat" => array(
-					"format" => "C*@ *@*"
-				),
-				"phoneFormats" => array(
-					"areaCodes" => array(782, 902)
-				)
-			)
-		),
-		array(
-			"regionName" => "Quebec",
-			"regionShort" => "QC",
-			"regionSlug" => "quebec",
-			"weight" => 23,
-			"cities" => array(
+			],
+			"extendedData": {
+				"zipFormat": {
+					"format": "C*@ *@*"
+				},
+				"phoneFormats": {
+					"areaCodes": [782, 902]
+				}
+			}
+		},
+		{
+			"regionName": "Quebec",
+			"regionShort": "QC",
+			"regionSlug": "quebec",
+			"weight": 23,
+			"cities": [
 				"Amqui", "Cabano", "Dégelis", "Price", "Rimouski", "Rivière-du-Loup", "Sainte-Flavie", "Alma", "Chambord",
 				"Chicoutimi", "La Baie", "Labrecque", "Saguenay", "Saint-Prime", "Shipshaw", "Baie-Saint-Paul", "Beauport",
 				"Cap-Rouge", "Deschambault", "Isle-aux-Coudres", "Lac-Serent", "Malbaie", "Neuville", "Pointe-au-Pic",
@@ -279,57 +272,51 @@ class Country_Canada extends CountryPlugin
 				"Thurso", "Dubuisson", "Malartic", "Notre-Dame-du-Nord", "Rouyn-Noranda", "Baie-Comeau", "Fermont",
 				"Kawawachikamach", "Matagami", "Caplan", "Carleton", "Gaspé", "Gespeg", "Maria", "Murdochville",
 				"Cap-Saint-Ignace", "Charny", "Lévis"
-			),
-			"extendedData" => array(
-				"zipFormat" => array(
-					"format" => "&*@ *@*"
-				),
-				"phoneFormat" => array(
-					"areaCodes" => array(418, 581, 450, 579, 514, 438, 819, 873)
-				)
-			)
-		),
-		array(
-			"regionName" => "Saskatchewan",
-			"regionShort" => "SK",
-			"regionSlug" => "saskatchewan",
-			"weight" => 4,
-			"cities" => array(
+			],
+			"extendedData": {
+				"zipFormat": {
+					"format": "&*@ *@*"
+				},
+				"phoneFormat": {
+					"areaCodes": [418, 581, 450, 579, 514, 438, 819, 873]
+				}
+			}
+		},
+		{
+			"regionName": "Saskatchewan",
+			"regionShort": "SK",
+			"regionSlug": "saskatchewan",
+			"weight": 4,
+			"cities": [
 				"Assiniboia", "Calder", "Canora", "Estevan", "Gravelbourg", "Hudson Bay", "Lang", "Langenburg", "Lloydminster",
 				"Macklin", "Maple Creek", "Milestone", "Moose Jaw", "North Battleford", "Prince Albert", "Regina", "Saskatoon",
 				"Weyburn", "Yorkton"
-			),
-			"extendedData" => array(
-				"zipFormat" => array(
-					"format" => "S*@ *@*"
-				),
-				"phoneFormat" => array(
-					"areaCodes" => array(306, 639)
-				)
-			)
-		),
-		array(
-			"regionName" => "Yukon",
-			"regionShort" => "YT",
-			"regionSlug" => "yukon",
-			"weight" => 1,
-			"cities" => array(
+			],
+			"extendedData": {
+				"zipFormat": {
+					"format": "S*@ *@*"
+				},
+				"phoneFormat": {
+					"areaCodes": [306, 639]
+				}
+			}
+		},
+		{
+			"regionName": "Yukon",
+			"regionShort": "YT",
+			"regionSlug": "yukon",
+			"weight": 1,
+			"cities": [
 				"Whitehorse", "Watson Lake"
-			),
-			"extendedData" => array(
-				"zipFormat" => array(
-					"format" => "Y*@ *@*"
-				),
-				"phoneFormat" => array(
-					"areaCodes" => array(867)
-				)
-			)
-		)
-	);
-
-
-	public function install()
-	{
-		return CountryPluginHelper::populateDB($this->countryName, $this->countrySlug, $this->countryData);
-	}
+			],
+			"extendedData": {
+				"zipFormat": {
+					"format": "Y*@ *@*"
+				},
+				"phoneFormat": {
+					"areaCodes": [867]
+				}
+			}
+		}
+	]
 }
