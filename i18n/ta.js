3098ef6d201721f725b36578e9b2f227f211d40f
Ben Keen <ben.keen@gmail.com>
2019-11-30 16:52:33 -0800

    main i18n files generated

---
+++ i18n/ta.js
@@ -0,0 +1,9 @@
+export default lang = {
+	"country": "நாடு",
+	"country_specific_data": "நாட்டை குறிக்கும் புள்ளிவிவரங்கள்",
+	"credit_card_data": "Credit Card Data",
+	"no": "இல்லை",
+	"refresh_page": "பக்கம் புதுப்பிப்பு",
+	"select_country": "நாட்டினை தேர்வுசெய்",
+	"yes": "ஆம்"
+};
