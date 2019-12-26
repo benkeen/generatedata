5001876a7f5f8d0d741b50f5ec93944991760b0f
Ben Keen <ben.keen@gmail.com>
2019-12-25 16:06:46 -0800

    Names, Alphanumeric now returning generate & getMetadata methods for Export Types

--- src/core/generator/generator.selectors.ts
+++ src/core/generator/generator.selectors.ts
@@ -15,14 +15,23 @@ export const getSortedRowsArray = createSelector(
 	(rows, sorted) => sorted.map((id: string) => ({ ...rows[id], id }))
 );
 
+export const getNonEmptySortedRows = createSelector(
+    getSortedRowsArray,
+    (rows) => rows.filter((row: any) => row.dataType !== null)
+);
 
 // TODO this would be in a separate bundle loaded async along with the export, dataType + country generation code
 export const getDataForExportType = createSelector(
-    getSortedRowsArray,
+    getNonEmptySortedRows,
     (rows) => {
+
+        console.log(rows);
+
         return {
             numResults: 500,
             template: rows.map(({ title, dataType, data }: any) => {
+
+                console.log('data type? ', dataType);
 
                 // TODO another assumption here. Maybe validate the whole component right-up front during the
                 // build step and throw a nice error saying what's missing
