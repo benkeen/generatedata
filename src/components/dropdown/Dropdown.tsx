40a7e524d1d40279cba24561551498b7de5fe00c
Ben Keen <ben.keen@gmail.com>
2020-05-02 22:05:11 -0700

    localization for country plugin UI

--- src/components/dropdown/Dropdown.tsx
+++ src/components/dropdown/Dropdown.tsx
@@ -26,19 +26,34 @@ const selectStyles = {
 
 const Dropdown = ({ value, isGrouped, options, placeholder, ...props }: any): JSX.Element => {
 	// react-select has a terrible API. You need to pass the entire selected object as the `value` prop to prefill it.
-	// instead, our component use the `value` prop, which is converted here
-	let selectedValue = '';
+	// That's not something you'd normally save - you just want to save a single value, because, well, duh. So to
+	// make it behave like a sane component, our component here just accepts a single `value` prop - which is either the
+	// single value for the field, or an array if it's isMulti. This code converts it to an object/array of objects
+	// for react-select
+	let selectedValue: any = '';
 	if (isGrouped) {
-		options.find((group: any) => {
-			const found = group.options.find((row: any) => row.value === value);
-			if (found && found !== -1) {
-				selectedValue = found;
+		if (props.isMulti) {
+			selectedValue = [];
+			options.filter((group: any) => {
+				// TODO
 				return true;
-			}
-			return false;
-		});
+			});
+		} else {
+			options.find((group: any) => {
+				const found = group.options.find((row: any) => row.value === value);
+				if (found && found !== -1) {
+					selectedValue = found;
+					return true;
+				}
+				return false;
+			});
+		}
 	} else {
-		selectedValue = options.find((row: any): any => row.value === value);
+		if (props.isMulti) {
+			selectedValue = options.filter((row: any): any => value.indexOf(row.value) !== -1);
+		} else {
+			selectedValue = options.find((row: any): any => row.value === value);
+		}
 	}
 
 	const placeholderStr = placeholder ? placeholder : 'Select...';
