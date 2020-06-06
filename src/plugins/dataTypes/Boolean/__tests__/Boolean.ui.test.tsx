ad9a45e5fde4a39df8ba7236110e7ed02251bedc
Ben Keen <ben.keen@gmail.com>
2020-06-05 21:07:50 -0700

    Remove test

--- src/plugins/dataTypes/Boolean/__tests__/Boolean.ui.test.tsx
+++ src/plugins/dataTypes/Boolean/__tests__/Boolean.ui.test.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
-import { render, fireEvent } from '@testing-library/react';
-import { initialState, Help, Options } from '../Boolean.ui';
+import { render } from '@testing-library/react';
+import { Help } from '../Boolean.ui';
 const i18n = require('../i18n/en.json');
 
 const defaultProps = {
@@ -10,33 +10,6 @@ const defaultProps = {
 	id: 'id',
 	dimensions: { width: 100, height: 100 }
 };
-
-describe('Options', () => {
-	it('changing the content should call callback', () => {
-		const data = { ...initialState };
-		const onUpdate = jest.fn();
-		const { container } = render(
-			<Options
-				{...defaultProps}
-				data={data}
-				onUpdate={onUpdate}
-			/>
-		);
-		const field = container.querySelector('input');
-
-		// @ts-ignore
-		fireEvent.change(field, {
-			target: {
-				value: 'new value!!'
-			}
-		});
-
-		expect(onUpdate).toBeCalledWith({
-			...initialState,
-			value: 'new value!!'
-		});
-	});
-});
 
 describe('Help', () => {
 	it('renders', () => {
