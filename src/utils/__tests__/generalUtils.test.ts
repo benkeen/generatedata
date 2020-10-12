43ed8b820ad181527145006dd884e176c6ee94d2
Ben Keen <ben.keen@gmail.com>
2020-10-11 18:57:15 -0700

    tests

--- src/utils/__tests__/generalUtils.test.ts
+++ src/utils/__tests__/generalUtils.test.ts
@@ -14,3 +14,29 @@ describe('isNumeric', () => {
         expect(generalUtils.isNumeric("blah")).toEqual(false);
     });
 });
+
+describe('isBoolean', () => {
+	it('evaluates as booleans', () => {
+		expect(generalUtils.isBoolean('one')).toEqual(false);
+		expect(generalUtils.isBoolean('true')).toEqual(false);
+		expect(generalUtils.isBoolean('false')).toEqual(false);
+		expect(generalUtils.isBoolean(false)).toEqual(true);
+		expect(generalUtils.isBoolean(true)).toEqual(true);
+		expect(generalUtils.isBoolean(1)).toEqual(false);
+		expect(generalUtils.isBoolean(0)).toEqual(false);
+		expect(generalUtils.isBoolean(undefined)).toEqual(false);
+		expect(generalUtils.isBoolean(null)).toEqual(false);
+		expect(generalUtils.isBoolean(NaN)).toEqual(false);
+	});
+});
+
+describe('cloneObj', () => {
+	it('clones an object', () => {
+		const obj = { one: 114 };
+		expect(obj === obj).toBeTruthy();
+
+		const clone: any = generalUtils.cloneObj(obj)
+		expect(obj === clone).toBeFalsy();
+		expect(obj.one === clone.one).toBeTruthy();
+	});
+});
