f4bd11ebcb404cb45bf872466c2079ff379ee559
Ben Keen <ben.keen@gmail.com>
2020-11-18 10:16:40 -0800

    server now communicating with DB

--- server/app.js
+++ server/app.js
@@ -1,9 +1,34 @@
 const express = require('express');
+const mysql = require('mysql');
 const port = process.env.PORT || 3001;
 const app = express();
 
+
 app.get('/api/here', (req, res) => {
-	res.json({ whoah: 'Holy crap API can be called!' });
+	const connection = mysql.createConnection({
+		host: 'db',
+		port: 3306,
+		user: 'root',
+		password: 'rootpass357',
+		database: 'generatedata'
+	});
+
+	connection.connect();
+
+	connection.query('SELECT * FROM users', (error, results) => {
+		if (error) {
+			console.log("error! YAYAYAYAY");
+			throw error;
+		}
+
+		const data = results.map((i) => i.name);
+
+		res.json({
+			data
+		});
+	});
+
+	connection.end();
 });
 
 app.listen(port);
--- server/db.sql
+++ server/db.sql
@@ -1,6 +1,5 @@
 CREATE DATABASE IF NOT EXISTS generatedata;
 
--- create the users for each database
 CREATE USER 'gduser'@'%' IDENTIFIED BY 'gdpassword';
 GRANT CREATE, ALTER, INDEX, LOCK TABLES, REFERENCES, UPDATE, DELETE, DROP, SELECT, INSERT ON `projectone`.* TO 'gduser'@'%';
 
