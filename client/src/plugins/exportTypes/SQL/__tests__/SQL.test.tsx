566eadb92a742a3ede30cc21f89057dd8f267c90
Ben Keen <ben.keen@gmail.com>
2020-11-24 09:56:45 -0800

    more depressing but more accurate coverage stats

--- client/src/plugins/exportTypes/SQL/__tests__/SQL.test.tsx
+++ client/src/plugins/exportTypes/SQL/__tests__/SQL.test.tsx
@@ -10,17 +10,17 @@ describe('getNumericFieldColumnIndexes', () => {
 
 		expect(getNumericFieldColumnIndexes([
 			{ title: 'Col 1', metadata: { general: { dataType: 'string' } }, dataType: 'Names' },
-			{ title: 'Col 1', metadata: { general: { dataType: 'string' } }, dataType: 'Phone' },
+			{ title: 'Col 2', metadata: { general: { dataType: 'string' } }, dataType: 'Phone' },
 		])).toEqual([]);
 	});
 
 	it('identifies numeric fields', () => {
 		expect(getNumericFieldColumnIndexes([
 			{ title: 'Col 1', metadata: { general: { dataType: 'number' } }, dataType: 'Names' },
-			{ title: 'Col 1', metadata: { general: { dataType: 'string' } }, dataType: 'Phone' },
-			{ title: 'Col 1', metadata: { general: { dataType: 'string' } }, dataType: 'Phone' },
-			{ title: 'Col 1', metadata: { general: { dataType: 'number' } }, dataType: 'List' },
-			{ title: 'Col 1', metadata: { general: { dataType: 'infer' } }, dataType: 'Phone' },
+			{ title: 'Col 2', metadata: { general: { dataType: 'string' } }, dataType: 'Phone' },
+			{ title: 'Col 3', metadata: { general: { dataType: 'string' } }, dataType: 'Phone' },
+			{ title: 'Col 4', metadata: { general: { dataType: 'number' } }, dataType: 'List' },
+			{ title: 'Col 5', metadata: { general: { dataType: 'infer' } }, dataType: 'Phone' },
 		])).toEqual([0, 3]);
 	});
 
