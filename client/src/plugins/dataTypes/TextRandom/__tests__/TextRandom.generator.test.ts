b9eab6ed5786a634272f5e32546925d3644607c4
Ben Keen <ben.keen@gmail.com>
2020-12-11 20:35:42 -0800

    more tests

---
+++ client/src/plugins/dataTypes/TextRandom/__tests__/TextRandom.generator.test.ts
@@ -0,0 +1,33 @@
+import sinon from 'sinon';
+import { onmessage } from '../TextRandom.generator';
+import utils from '../../../../utils';
+import { getBlankDTGeneratorPayload } from '../../../../../tests/testHelpers';
+
+describe('onmessage', () => {
+	const postMessage = jest.fn();
+	const importScripts = jest.fn();
+	beforeAll(() => {
+		window.postMessage = postMessage;
+		window.importScripts = importScripts;
+	});
+
+	it('generates random data', () => {
+		const payload: any = {
+			data: {
+				...getBlankDTGeneratorPayload(),
+				rowState: {
+					numWordsToGenerate: 1,
+					fromStart: 1,
+					minWords: 1,
+					maxWords: 1,
+					words: ['word']
+				}
+			}
+		};
+
+		sinon.stub(utils.randomUtils, 'generateRandomTextStr').returns('testXXX');
+
+		onmessage(payload);
+		expect(postMessage).toHaveBeenCalledWith({ display: 'testXXX' });
+	});
+});
