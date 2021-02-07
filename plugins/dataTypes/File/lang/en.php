b9ef7340f41b9df88c6d7a9b4143196241555d87
Enso studio <info@ensostudio.ru>
2021-02-07 23:44:36 +0300

    Add DataTypePlugin File

---
+++ plugins/dataTypes/File/lang/en.php
@@ -0,0 +1,14 @@
+<?php
+
+$L = array(
+    "DATA_TYPE" => array(
+        "NAME" => "File",
+        "DESC" => "Generates a random filename.",
+    ),
+    "path" => "Path/prefix",
+    "extensions" => "Extension(s)",
+    "length from" => "Length from",
+    "to" => "to",
+    "symbols" => "symbols",
+    "help" => "<p>This option generates a random filename.</p>"
+);
