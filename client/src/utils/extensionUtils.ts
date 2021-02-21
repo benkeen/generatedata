ddc99a264adfec4f489b2cd2a9166c0f4e5d120d
Ben Keen <ben.keen@gmail.com>
2021-02-21 12:06:13 -0800

    extension utils rename

--- client/src/utils/extensionUtils.ts
+++
@@ -1,9 +0,0 @@
-const customFooterLinks: JSX.Element[] = [];
-
-// to keep things simple, a registered footer link should be wrapped in it's own <li>. That way it can
-// supply any additional styling for the whole link externally.
-export const registerCustomFooterLink = (link: JSX.Element): void => {
-	customFooterLinks.push(link);
-};
-
-export const getCustomFooterLinks = (): JSX.Element[] => customFooterLinks;
