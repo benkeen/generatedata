2071d472a078ed61128342e4f9b0edba8891f9f4
Ben Keen <ben.keen@gmail.com>
2021-09-26 16:57:55 -0700

    clearer emphasis of selected lang in lang tooltip

--- client/_localeFileMap.ts
+++ client/_localeFileMap.ts
@@ -1,14 +1,14 @@
 import { LocaleFileMap } from '~types/general';
 
 export const localeFileMap: LocaleFileMap = {
-	"ar": "ar-8768b6a2.js",
-	"de": "de-2ac819e4.js",
-	"en": "en-df2d2c68.js",
-	"es": "es-3e5213ae.js",
-	"fr": "fr-9576458f.js",
-	"ja": "ja-91e0917f.js",
-	"hi": "hi-d99e2997.js",
-	"nl": "nl-61dd60cc.js",
-	"ta": "ta-e09c2791.js",
-	"zh": "zh-bb1296f9.js"
+	"ar": "ar-c38fe6c5.js",
+	"de": "de-4c96fff1.js",
+	"en": "en-ad2c6016.js",
+	"es": "es-b5ae6f51.js",
+	"fr": "fr-34e69e6e.js",
+	"ja": "ja-6e2cae91.js",
+	"hi": "hi-ec7dfc60.js",
+	"nl": "nl-25097477.js",
+	"ta": "ta-51996d0e.js",
+	"zh": "zh-a691c0d3.js"
 };
