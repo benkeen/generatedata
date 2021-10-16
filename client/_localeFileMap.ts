a1a15aadeee859e878fb6c516055f432c3ccaab5
Ben Keen <ben.keen@gmail.com>
2021-10-15 19:40:13 -0700

    regional names, continued

--- client/_localeFileMap.ts
+++ client/_localeFileMap.ts
@@ -2,13 +2,13 @@ import { LocaleFileMap } from '~types/general';
 
 export const localeFileMap: LocaleFileMap = {
 	"ar": "ar-4320ec0b.js",
-	"de": "de-0fb8ff1d.js",
-	"en": "en-089ef91c.js",
-	"es": "es-9d998bea.js",
-	"fr": "fr-da162240.js",
+	"de": "de-8d69dd2d.js",
+	"en": "en-a50c8688.js",
+	"es": "es-c697bca8.js",
+	"fr": "fr-974d9aa1.js",
 	"hi": "hi-2fc1ad8d.js",
 	"ja": "ja-8b248356.js",
-	"nl": "nl-8939ff52.js",
+	"nl": "nl-f4487c6f.js",
 	"pt": "pt-647cdf41.js",
 	"ta": "ta-a4f6abeb.js",
 	"zh": "zh-905e9e92.js"
