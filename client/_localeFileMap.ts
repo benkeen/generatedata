7dc85a79d0668c5dbc780486207b6d697c7006ae
Ben Keen <ben.keen@gmail.com>
2022-11-26 15:11:14 -0800

    WeightedList translations

--- client/_localeFileMap.ts
+++ client/_localeFileMap.ts
@@ -1,15 +1,15 @@
 import { GDLocaleMap } from '~types/general';
 
 export const localeFileMap: GDLocaleMap = {
-	"ar": "ar-531cbf91.js",
-	"de": "de-08061372.js",
-	"en": "en-82f1bc1b.js",
-	"es": "es-9e326cfd.js",
-	"fr": "fr-9e525864.js",
-	"hi": "hi-50946462.js",
-	"ja": "ja-9a916444.js",
-	"nl": "nl-693010cf.js",
-	"pt": "pt-0cfc8e73.js",
-	"ta": "ta-73a72931.js",
-	"zh": "zh-007e40bb.js"
+	"ar": "ar-e6bb3201.js",
+	"de": "de-e6bec895.js",
+	"en": "en-4d0faf64.js",
+	"es": "es-35afd3f2.js",
+	"fr": "fr-7d2076bc.js",
+	"hi": "hi-d99e8d62.js",
+	"ja": "ja-916f3d54.js",
+	"nl": "nl-a152518d.js",
+	"pt": "pt-1dcee384.js",
+	"ta": "ta-ae595074.js",
+	"zh": "zh-40210d2b.js"
 };
