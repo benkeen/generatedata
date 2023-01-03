71be85357862d32dc43455376a3b1a22eb533084
Ben Keen <ben.keen@gmail.com>
2023-01-02 22:27:27 -0800

    adding tests for bin file args

--- standalone/src/utils/__tests__/bin.test.ts
+++ standalone/src/utils/__tests__/bin.test.ts
@@ -1,7 +1,46 @@
-import { getConfigFile } from '../bin';
+import { getConfigFile, msgs } from '../bin';
 
 describe('getConfigFile', () => {
-	it('.', () => {
-		expect(true).toBe(true);
+	const stubExit = () => {};
+
+	describe('--config param error states', () => {
+		const error = console.error;
+
+		beforeEach(() => {
+			console.error = jest.fn();
+		});
+
+		afterAll(() => {
+			console.error = error;
+		});
+
+		it('logs an error when --config param missing', () => {
+			getConfigFile({}, stubExit);
+			expect(console.error).toHaveBeenCalledWith(msgs.missingConfigArg);
+		});
+
+		it('logs an error when --config param value is not a valid JSON file', () => {
+			getConfigFile({
+				config: 'config.xml'
+			}, stubExit);
+			expect(console.error).toHaveBeenCalledWith(msgs.invalidConfigFileFormat);
+		});
+
+		it('logs an error when --config param value is set properly but file does not exist', () => {
+			getConfigFile({
+				config: '__tests__/invalid-config.json'
+			}, stubExit);
+			expect(console.error).toHaveBeenCalledWith(msgs.invalidConfigFileContent);
+		});
+	});
+
+	describe('returns the config file content ', () => {
+		it('logs an error when --config param missing', () => {
+			const configFile = getConfigFile({
+				config: '__tests__/valid-config.json'
+			}, stubExit);
+
+			expect(configFile).toEqual({ valid: true });
+		});
 	});
 });
--- standalone/src/utils/bin.ts
+++ standalone/src/utils/bin.ts
@@ -2,35 +2,46 @@ import path from 'path';
 import fs from 'fs';
 
 type BinCommandArgs = {
-    config: string;
+    config?: string;
 }
+
+export const msgs = {
+    missingConfigArg: 'Missing --config argument.',
+    invalidConfigFileFormat: 'Invalid configuration file. Please supply a path to a .json file',
+    configFileNotFound: 'Unable to find configuration file at this location: ',
+    invalidConfigFileContent: 'Unable to read configuration file. Please check it\'s a valid JSON file.'
+};
 
 export const getConfigFile = (args: BinCommandArgs, exit: any) => {
     // check for required --config parameter
     if (!args.config) {
-        console.error('Missing --config argument.');
+        console.error(msgs.missingConfigArg);
         exit(1);
+        return;
     }
 
     // check it's a JSON file
-    if (!(/.json$/.test(args.config))) {
-        console.error('Invalid configuration file. Please supply a path to a .json file.');
+    if (!(/.json$/.test(args.config || ''))) {
+        console.error(msgs.invalidConfigFileFormat);
         exit(1);
+        return;
     }
 
     // check the file exists
-    const configFile = path.resolve(__dirname, args.config);
+    const configFile = path.resolve(__dirname, args.config || '');
     if (!fs.existsSync(configFile)) {
-        console.error('Unable to find configuration file at this location: ', configFile);
+        console.error(msgs.configFileNotFound, configFile);
         exit(1);
+        return;
     }
 
     let configFileContent;
     try {
         configFileContent = require(configFile);
     } catch (e) {
-        console.error('Unable to read configuration file. Please check it\'s a valid JSON file');
+        console.error(msgs.invalidConfigFileContent);
         exit(1);
+        return;
     }
 
     return configFileContent;
